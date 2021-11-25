3805e2cb8988a5380542a7d5ae85ecbb6bfb3a48
Van Nguyen <npnv.vn1@gmail.com>
2021-11-25 14:21:05 +0700

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 const app = express();
+const port = process.env.PORT || 3000;
 // Defines path
 const publicDirectoryPath = path.join(__dirname, '../public');
 const viewPath = path.join(__dirname, '../templates/views');
@@ -98,6 +99,6 @@ app.get('*', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Connect to port!');
+app.listen(port, () => {
+  console.log(`Connect to port ${port}!`);
 });
